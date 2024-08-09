dccdb9f24cd99bf6079b0c16c6006b33746a77ac
Kararasenok =) <murzikkurzikpro@gmail.com>
2024-08-09 03:49:08 +0300

    FIRST REALESE AFTER 6 HOURS YEEEEEEE
    FINALLY I DO IT 🦅🦅

---
+++ README.md
@@ -0,0 +1,41 @@
+# Lyntr Account Switcher
+Simple Extension that allow to switch between accounts in [Lyntr](https://lyntr.com/)
+
+## Installation
+## Local
+1. Download ZIP of project
+2. Unzip it somewhere
+3. In your browser open
+```url
+chrome://extensions/
+```
+
+4. Select "Load unpacked extension"
+5. Select unzipped folder
+Or
+4. Drag and drop unzipped folder in extensions tab
+
+**Done!**
+
+## Usage
+1. In your browser open "Lyntr Account Switcher" and [Lyntr](https://lyntr.com/)
+2. Add your account (Buttons **Add Account** and **Add This Account (already has inserted token of account)**)
+3. Fill all fields and click **Save and Close**
+4. Then press **Select** button opposite the display name
+
+**Done!**
+
+## GIF Guides
+
+### Installing the extension
+#### Loding unpacked extension
+![Installing the extension Method 1](https://raw.githubusercontent.com/kararasenok-gd/lyntr-account-switcher/main/assets/guides/select.gif)
+#### Drag and drop
+![Installing the extension Method 2](https://raw.githubusercontent.com/kararasenok-gd/lyntr-account-switcher/main/assets/guides/dragndrop.gif)
+### Adding accounts
+#### Current
+![Adding accounts](https://raw.githubusercontent.com/kararasenok-gd/lyntr-account-switcher/main/assets/guides/current.gif)
+#### Empty
+![Adding accounts](https://raw.githubusercontent.com/kararasenok-gd/lyntr-account-switcher/main/assets/guides/empty.gif)
+### Switching accounts
+![Switching accounts](https://raw.githubusercontent.com/kararasenok-gd/lyntr-account-switcher/main/assets/guides/switching.gif)
---
+++ content.js
@@ -0,0 +1,135 @@
+(function () {
+    const container = document.getElementById('container');
+    const accountList = document.getElementById('accountList');
+    const addAccountButton = document.getElementById('addAccountButton');
+    const addThisAccountButton = document.getElementById('addThisAccountButton');
+    const accountSettings = document.getElementById('accountSettings');
+    const accountDisplayNameInput = document.getElementById('accountDisplayName');
+    const accountTokenInput = document.getElementById('accountToken');
+    const accountSettingsSubmit = document.getElementById('accountSettingsSubmit');
+    const deleteAccountButton = document.getElementById('deleteAccountButton');
+    let currentEditIndex = null;
+    let currentToken = null;
+
+    function fetchCurrentToken(callback) {
+        chrome.cookies.get({ url: 'https://lyntr.com', name: '_TOKEN__DO_NOT_SHARE' }, (cookie) => {
+            if (cookie) {
+                currentToken = cookie.value;
+                callback();
+            } else {
+                callback();
+                console.log('Token not found!');
+                callback();
+            }
+        });
+    }
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (arrayOfTabs) {
+        if (!arrayOfTabs[0].url.startsWith('https://lyntr.com')) {
+            container.innerHTML = `<h1>FIRSTLY OPEN LYNTR</h1>`;
+            throw new Error('Lyntr is not open');
+        }
+    });
+
+    function updateAccountList() {
+        accountList.innerHTML = '';
+        const accounts = JSON.parse(localStorage.getItem('lyntrAccounts')) || [];
+        accounts.forEach((account, index) => {
+            const accountDiv = document.createElement('div');
+            const selectButton = document.createElement('button');
+            let currentAccount = false;
+            if (account.token === currentToken) {
+                accountDiv.classList.add('currentAccount');
+                currentAccount = true;
+            }
+            selectButton.textContent = 'Select';
+            selectButton.onclick = () => selectAccount(index);
+            selectButton.disabled = currentAccount;
+            accountDiv.textContent = account.displayName + ' ';
+
+            accountDiv.appendChild(selectButton);
+
+            const settingsButton = document.createElement('button');
+            settingsButton.textContent = '⚙️';
+            settingsButton.onclick = () => openAccountSettings(index);
+            accountDiv.appendChild(settingsButton);
+
+            accountList.appendChild(accountDiv);
+        });
+    }
+
+    addAccountButton.onclick = () => {
+        const accounts = JSON.parse(localStorage.getItem('lyntrAccounts')) || [];
+        accounts.push({ displayName: '', token: '' });
+        localStorage.setItem('lyntrAccounts', JSON.stringify(accounts));
+        currentEditIndex = accounts.length - 1;
+        openAccountSettings(currentEditIndex, false);
+        openAccountSettings(currentEditIndex, false);
+    };
+
+    addThisAccountButton.onclick = () => {
+        chrome.cookies.get({ url: 'https://lyntr.com', name: '_TOKEN__DO_NOT_SHARE' }, (cookie) => {
+            if (cookie) {
+                const token = cookie.value;
+                const accounts = JSON.parse(localStorage.getItem('lyntrAccounts')) || [];
+                accounts.push({ displayName: '', token });
+                localStorage.setItem('lyntrAccounts', JSON.stringify(accounts));
+                currentEditIndex = accounts.length - 1;
+                openAccountSettings(currentEditIndex, true);
+                openAccountSettings(currentEditIndex, true);
+            } else {
+                alert('Token not found!');
+            }
+        });
+    };
+
+    function selectAccount(index) {
+        const accounts = JSON.parse(localStorage.getItem('lyntrAccounts'));
+        const selectedAccount = accounts[index];
+        chrome.cookies.set({ url: 'https://lyntr.com', name: '_TOKEN__DO_NOT_SHARE', value: selectedAccount.token }, () => {
+            alert(`Account ${selectedAccount.displayName} selected!\n\nIf something broken, try reload page again. If it still doesn't work, report a bug here: https://github.com/kararasenok-gd/lyntr-account-switcher/issues`);
+            window.location.reload();
+            chrome.tabs.query({ active: true, currentWindow: true }, function (arrayOfTabs) { chrome.tabs.reload(arrayOfTabs[0].id); });
+        });
+    }
+
+    function openAccountSettings(index, isThisAccount) {
+        const accounts = JSON.parse(localStorage.getItem('lyntrAccounts'));
+        currentEditIndex = index;
+        if (isThisAccount) {
+            accountDisplayNameInput.value = '';
+        } else {
+            accountDisplayNameInput.value = accounts[index].displayName; 
+        }
+        accountTokenInput.value = accounts[index].token;
+        accountSettings.style.display = 'block';
+    }
+
+    accountSettingsSubmit.onclick = () => {
+        const accounts = JSON.parse(localStorage.getItem('lyntrAccounts'));
+        accounts[currentEditIndex].displayName = accountDisplayNameInput.value;
+        accounts[currentEditIndex].token = accountTokenInput.value;
+        localStorage.setItem('lyntrAccounts', JSON.stringify(accounts));
+        accountSettings.style.display = 'none';
+        updateAccountList();
+    };
+
+    deleteAccountButton.onclick = () => {
+        const accounts = JSON.parse(localStorage.getItem('lyntrAccounts'));
+        accounts.splice(currentEditIndex, 1);
+        localStorage.setItem('lyntrAccounts', JSON.stringify(accounts));
+        accountSettings.style.display = 'none';
+        updateAccountList();
+    };
+
+    fetchCurrentToken(updateAccountList);
+
+})();
+
+document.getElementById('accountTokenShow').addEventListener('change', function () {
+    if (this.checked) {
+        document.getElementById('accountToken').type = 'text';
+    } else {
+        document.getElementById('accountToken').type = 'password';
+    }
+})
